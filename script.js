b9956f07e0a3d59651a6fc97f64467b9a2ba41d5
Angounou <jcangounou@hotmail.fr>
2023-01-12 06:00:16 +0100

    debug

---
+++ script.js
@@ -0,0 +1,21 @@
+// window.document.onload() {
+
+
+// };
+const corps = document.querySelector('body');
+const nav = document.querySelector('nav');
+nav.classList.add('test');
+nav.innerText = 'super test';
+newDiv = document.createElement('corps').append('div');
+
+
+// function addBlocProdts () { 
+
+    
+//         zoneProduit = document.querySelector('section.items');
+//         blocProduit = document.createElement('zoneProduit').append('a');
+//         articleProduit = document.querySelector('blocProduit');
+//         document.createElement('blocProduit').append('article');
+//         console.log (document.querySelector(article));
+//         return article;
+// };
